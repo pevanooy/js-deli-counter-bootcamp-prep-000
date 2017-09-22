7f574cc3a00a9290e104c97f3f8f2dc47981e4d4
Peter van Ooy <pevanooy@gmail.com>
2017-09-22 13:10:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var katzDeliLine = [];
 function takeANumber(katzDeliLine, newPerson){
   katzDeliLine.push(newPerson);
   retString = "Welcome, " + newPerson.toString() + ". You are number " + katzDeliLine.length.toString() + " in Line."
-  retrun retString  
+  retrun retString
 }
 
 function nowServing(){
-  
+
 }
 
 function currentLine(){
-  
+
 }
